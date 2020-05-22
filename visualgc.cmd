57794468153bfbe1596093eb92efc3f2ebeb22ab
Jacky Liu <liucj@i-counting.cn>
2020-05-22 18:11:25 +0800

    visualgc patch java 5, 6, 7, 8, 14 tested

--- visualgc.cmd
+++ visualgc.cmd
@@ -1,2 +1,4 @@
 cd "%cd%"
-java -cp "%JAVA_HOME%\lib\tools.jar"; ./lib/visualgc.jar:./lib/visualgc_patch.jar com.sun.jvmstat.tools.visualgc.VisualGCPatch
+set JAVA_HOME=C:\Java\jdk1.5
+"%JAVA_HOME%\bin\java" -cp "%JAVA_HOME%\lib\tools.jar";.\lib\visualgc.jar;.\lib\visualgc_patch.jar com.sun.jvmstat.tools.visualgc.VisualGCPatch
+pause
