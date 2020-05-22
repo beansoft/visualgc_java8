d9f8f882b91ceeb55b80c974d4ec7f2897d1355b
liuchangjiong <liucj@i-counting.cn>
2020-05-22 23:47:59 +0800

    update shell

--- visualgc.cmd
+++ visualgc.cmd
@@ -1,4 +1,4 @@
 cd "%cd%"
-set JAVA_HOME=C:\Java\jdk1.5
+rem set JAVA_HOME=C:\Java\jdk1.8.0_252
 "%JAVA_HOME%\bin\java" -cp "%JAVA_HOME%\lib\tools.jar";.\lib\visualgc.jar;.\lib\visualgc_patch.jar com.sun.jvmstat.tools.visualgc.VisualGCPatch
 pause
