d54cff31654a95e7098b7d544409e3841c49dfa1
liuchangjiong <liuchangjiong@huimin100.cn>
2021-02-19 20:15:28 +0800

    fix Non-extendable class BundleBase is extended by DevkitHelperBundle. This class is marked with @ApiStatus.NonExtendable, which indicates that the class is not supposed to be extended. See documentation of the @ApiStatus.NonExtendable for more info.

--- devkit-helper/src/com/github/beansoft/devkit/DevkitHelperBundle.java
+++ devkit-helper/src/com/github/beansoft/devkit/DevkitHelperBundle.java
@@ -1,21 +1,35 @@
 package com.github.beansoft.devkit;
 
+import com.intellij.AbstractBundle;
 import com.intellij.BundleBase;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
 
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
 import java.util.ResourceBundle;
 
-public final class DevkitHelperBundle extends BundleBase {
+public final class DevkitHelperBundle {
   @NonNls public static final String BUNDLE = "messages.DevkitHelperBundle";
-  private static ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE);
 
   private DevkitHelperBundle() {
   }
 
+  private static Reference<ResourceBundle> ourBundle;
+
   @NotNull
-  public static String message(@NotNull String key, @NotNull Object  ... params) {
-    return message(bundle, key, params);
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return AbstractBundle.message(getBundle(), key, params);// 修正 by BeanSoft 去掉旧API调用
+  }
+
+  private static ResourceBundle getBundle() {
+    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
+    if (bundle == null) {
+      bundle = ResourceBundle.getBundle(BUNDLE);
+      ourBundle = new SoftReference<>(bundle);
+    }
+    return bundle;
   }
 
 }
